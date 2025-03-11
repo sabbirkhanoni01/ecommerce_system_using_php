fd7c97b313bc02d0ca002e5e92d9b5ce450bc571
sabbirkhanoni <mdsabbirkhanoni90@gmail.com>
2025-03-11 21:35:28 +0600

    first commit

---
+++ customer_registration_view/register_submitted.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Submission Successful</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                <h2>Thank you for Registering.</h2>
+                <p>Form has been submitted successfully.</p>
+                <p><a href="register.php">Go Back to Register</a></p>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
